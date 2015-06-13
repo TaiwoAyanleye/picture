7b0e8a9cadda6207eff22bd2bb06e55fbec16981
tye <TaiwoAyanleye@users.noreply.github.com>
2015-06-13 14:47:29 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function start () {
+	var nwname = document.getElementById("nwname");
+	var nwemail= document.getElementById("nwemail")
+	var nwpswrd= document.getElementById("nwpswrd")
+	var create= document.getElementById("create")
+	var usname= document.getElementById("usname")
+	var uspswrd= document.getElementById("uspswrd")
+	var log_in= document.getElementById("log_in")
+}
+function events () {
+	retVal = confirm("Do you want to continue ?");
+	if( retVal == true ){
+	alert("User wants to continue!");
+	}else{
+	alert("User does not want to continue!");
+	plat;
+	return false;
+	}
+	
+}
+window.addEventListner("load", start, false)
+
+
